8b534f1a5e2aab150c4615c0e6dcd38da93ff13f
codec <codec@fnord.cx>
2013-01-20 20:02:11 +0100

    ensure the webrick master is stopped
    this is due to https://github.com/puppetlabs-operations/puppet-puppet/blob/master/manifests/server.pp#L107.
    should only be installed in case of servertype => standalone

--- manifests/server/passenger/debian.pp
+++ manifests/server/passenger/debian.pp
@@ -15,4 +15,9 @@ class puppet::server::passenger::debian {
     ensure  => present,
     require => Apt::Source['puppetlabs']
   }
+
+  # workaround an issue in puppet::server
+  # where the puppetmaster package is installed
+  # for all server types
+  service { 'puppetmaster': ensure => stopped, enable => false }
 }
